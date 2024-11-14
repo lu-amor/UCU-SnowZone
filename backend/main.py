00fc5eda700c804cf4ec4fe8a8f5b4cca4d90b8c
luispee <lpgaudiano99@gmail.com>
2024-11-14 14:48:44 +0100

    create/delete alumno

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 from flask_cors import CORS
-import json
+from config import app, get_db_connection
 
 app = Flask(__name__)
 
@@ -37,6 +37,57 @@ def login():
             return jsonify({"message": "Invalid username or password", "status": "error"}), 401
 
     return render_template('login.html')
+
+@app.route("/students", methods=["GET"])
+def get_contacts():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM alumno")
+        students = cursor.fetchall()
+        #json_students = list(map(lambda x: x.to_json(), students))
+        cursor.close()
+        connection.close()
+        return jsonify({"students": students})
+    except Exception as e:
+        return (
+            jsonify({"message": str(e)}), 400
+        )
+        
+
+@app.route("/create_student", methods=["POST"])
+def create_student():
+    cedula = request.json.get("ci")
+    nombre = request.json.get("nombre")
+    apellido = request.json.get("apellido")
+    f_nac = request.json.get("f_nac")
+    mail = request.json.get("mail")
+    tel = request.json.get("tel")
+
+    if not cedula or not nombre or not apellido or not f_nac or not mail or not tel:
+        return (
+            jsonify({"message": "Completar informacion del estudiante"}),
+            400,
+        )
+    
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        new_student = """
+            INSERT INTO alumno (ci, nombre, apellido, f_nac, mail, tel)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(new_student, (cedula, nombre, apellido, f_nac, mail, tel))
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+    except Exception as e:
+        return jsonify({"message": str(e)}),400
+
+    return jsonify({"message": "Alumno creado correctamente"}), 201
 
 @app.route('/logout')
 @login_required
